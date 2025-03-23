9397505855ca20a6a3707af8b0387ceb4306fac5
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2025-03-23 13:02:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install -y python3-pip
-export PATH="$PATH:~/.local/bin"
+sudo apt install -y python3-pip python3-venv python3-is-python
+python -m venv ee
+source ee/bin/activate
 pip install -r requirements.txt
-sudo apt install -y python3-opencv
+sudo apt install nginx
